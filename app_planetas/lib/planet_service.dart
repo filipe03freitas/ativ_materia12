8b8d257de202c6a8dd922c3ccbc9472b40b0f65e
Filipe Freitas <51628926+filipe03freitas@users.noreply.github.com>
2025-02-11 19:40:45 -0300

    Update planet_service.dart

--- app_planetas/lib/planet_service.dart
+++ app_planetas/lib/planet_service.dart
@@ -7,18 +7,16 @@
 
   Planet({this.id, required this.name, required this.distance, required this.size, this.nickname});
 
-  // Converte um Mapa do banco de dados para um objeto Planet
   factory Planet.fromMap(Map<String, dynamic> map) {
     return Planet(
       id: map['id'],
       name: map['name'],
-      distance: map['distance'].toDouble(), // Garante que é double
-      size: map['size'].toDouble(), // Garante que é double
+      distance: map['distance'].toDouble(), 
+      size: map['size'].toDouble(),
       nickname: map['nickname'],
     );
   }
 
-  // Converte um objeto Planet para um Mapa para salvar no banco
   Map<String, dynamic> toMap() {
     return {
       'id': id,
